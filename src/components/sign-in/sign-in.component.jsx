82498c6e622daaf2130e0bfdb1ceee199fff173f
LENSPIRA <lenspira@outlook.com>
2021-09-25 14:06:37 -0500

    implement firebase utils, include ability to store authenticated users into firestore database

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -53,7 +53,7 @@ class SignIn extends React.Component {
           <div className="buttons">
             <CustomButton type="submit">Sign In</CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-              Sign In With Google
+              Sign in with Google
             </CustomButton>
           </div>
         </form>
